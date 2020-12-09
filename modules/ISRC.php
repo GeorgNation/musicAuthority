b13df3c7d52d1842587a08c114da79e2ef21e5c3
GeorgNation <55389952+GeorgNation@users.noreply.github.com>
2020-12-09 11:52:07 +0600

    Update ISRC.php

--- modules/ISRC.php
+++ modules/ISRC.php
@@ -1,35 +1,69 @@
 <?php
 
-class ISRC
+class UPC
 {
-	protected $country;  // Страна
-	protected $registar; // Регистратор
-	protected $year;     // Год
-	public $number;   // Номер трека
+	protected $issuerCode;
+	public $number;
 
-	public function __construct ($country = "RU", $registar = "M0D") // Создание класса
+	public function __construct ($issuerCode, $number = 1)
 	{
-		$this->country = $country;
-		$this->registar = $registar;
-		$this->year = date ("y");
-		$this->number = 1;
+		$this->issuerCode = (int)$issuerCode;
+		$this->number = $number;
 	}
 
-	public function parse ($isrc) //Парсинг ISRC
+	public function parse ($upc) //Парсинг upc
 	{
-		if (strlen ($isrc) == 12)
+		if (strlen ($upc) == 12)
 		{
-			$isrc = str_split ($isrc, 1);
+			$upc = str_split ($upc, 1);
 
 			return array (
-				"country" => $isrc[0].$isrc[1],
-				"registar" => $isrc[2].$isrc[3].$isrc[4],
-				"year" => intval ($isrc[5].$isrc[6]),
-				"number" => intval ($isrc[7].$isrc[8].$isrc[9].$isrc[10].$isrc[11])
+				"type" => intval ($upc[0]),
+				"issuer" => intval ($upc[1].$upc[2].$upc[3].$upc[4].$upc[5]),
+				"number" => intval ($upc[6].$upc[7].$upc[8].$upc[9].$upc[10])
 			);
 		}
 		else
 			return false;
+	}
+
+	protected static function calculateChecksum($data) // https://github.com/SamMakesCode/upc/blob/master/src/UPC.php
+    {
+        // Get all the individual digits
+        $digits = str_split($data);
+
+        // Create variables to store sums
+        $even_digit_sum = 0;
+        $odd_digit_sum = 0;
+
+        // For each digit
+        // Bear in mind, we start counting from 0, so odd and even operations are swapped here
+        for ($i = 0; $i < count($digits); $i++) {
+            if ($i % 2 === 0) {
+                // If it's even
+                $odd_digit_sum += $digits[$i];
+            } else {
+                // If it's odd
+                $even_digit_sum += $digits[$i];
+            }
+        }
+
+        // Multiply the sum of the odd digits by 3
+        $odd_digit_sum_multiplied = $odd_digit_sum * 3;
+
+        // Add the sums together
+        $sum_of_both = $odd_digit_sum_multiplied + $even_digit_sum;
+
+        // Get the remainder when divided by 10
+        $remainder = $sum_of_both % 10;
+
+        // If it's not 0, minus it from 10
+        if ($remainder !== 0) {
+            return 10 - $remainder;
+        }
+
+        // Return 0 remainder
+        return $remainder;
 	}
 
 	public function next ()
@@ -42,8 +76,9 @@ class ISRC
 		--$this->number;
 	}
 
-	public function get () // Получить ISRC код
+	public function __toString ()
 	{
-		return $this->country . $this->registar . $this->year . str_pad ($this->number, 5, "0", STR_PAD_LEFT);
+		return "7" . $this->issuerCode . str_pad ($this->number, 5, "0", STR_PAD_LEFT) . $this->calculateChecksum ("7" . $this->issuerCode . str_pad ($this->number, 5, "0", STR_PAD_LEFT));
 	}
+
 }
